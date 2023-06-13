f03a57d6195bdda8d14edd79802fe5c16d08e48a
phuongnam <phuongnam081201@gmail.com>
2023-06-13 21:54:44 +0700

    chỉnh provider

--- src/accounts/services/accounts.service.ts
+++ src/accounts/services/accounts.service.ts
@@ -7,6 +7,8 @@ import { CreateAccountDto } from '@accounts/dto';
 import { ServiceProvider } from '@service-providers/entities/service-provider.entity';
 import { ServiceProviderRepository } from '@service-providers/repositories/service-provider.repository';
 import { nonAccentVietnamese } from 'src/utils/string';
+import { AddressRepository } from '@addresses/repositories/address.repository';
+import { Address } from '@addresses/entities/address.entity';
 
 @Injectable()
 export class AccountsService {
@@ -15,6 +17,8 @@ export class AccountsService {
     private readonly accountRepository: AccountRepository,
     @InjectRepository(ServiceProvider)
     private readonly providerRepository: ServiceProviderRepository,
+    @InjectRepository(Address)
+    private readonly adrressRepository: AddressRepository,
   ) {}
 
   async create(createAccountDto: CreateAccountDto) {
@@ -33,9 +37,18 @@ export class AccountsService {
     });
 
     const savedAccount = await this.accountRepository.save(account);
+    const addresses = await this.adrressRepository.find({
+      select: { id: true },
+    });
 
     const serviceProvider = this.providerRepository.create({
-      account: savedAccount,
+      account: {
+        id: savedAccount.id,
+      },
+      code: username,
+      address: {
+        id: (Math.floor(Math.random() * addresses.length) + 1).toString(),
+      },
     });
 
     await this.providerRepository.save(serviceProvider);
